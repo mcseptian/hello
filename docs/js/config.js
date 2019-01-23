5713d4305df5620cd0c423db663508a0752c20b7
Septian Adi <mcseptian007@gmail.com>
2019-01-23 13:20:32 +0700

    errorDirectory

--- docs/js/config.js
+++ docs/js/config.js
@@ -16,7 +16,7 @@ var config = {
     branch: 'master',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
-    // prefix: 'subdirectory',
+    prefix: 'docs',
   },
 
   // The name of the layouts directory.
