ca7e42844d47e1a39cc13be9f94bdbf1c8be78da
Septian Adi <18345339+mcseptian@users.noreply.github.com>
2019-04-10 17:53:12 +0700

    Update config.js

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,50 +1,63 @@
-// Config
-var config = {
+$(function() {
 
-  // ID of element to attach CMS.js to
-  elementId: 'cms',
+	CMS.init({
 
-  // Mode 'GITHUB' for Github Pages, 'SERVER' for Self Hosted
-  // Defaults to Server mode if not specified
-  mode: 'GITHUB',
+		// Name of your site or location of logo file ,relative to root directory (img/logo.png)
+		siteName: 'Project one',
 
-  // If Github mode is set, your Github username, repo name, 
-  // and branch to get files from.
-  github: {
-    username: 'mcseptian',
-    repo: 'https://github.com/mcseptian/hello',
-    branch: 'master',
-    host: 'https://api.github.com',
-    // Use prefix option if your site is located in a subdirectory.
-    // prefix: 'subdirectory',
-  },
+		// Tagline for your site
+		siteTagline: '2 weeks sprint',
 
-  // The name of the layouts directory.
-  layoutDirectory: 'layouts',
+		// Email address
+		siteEmail: 'mcseptian007@gmail.com',
 
-  // The error layout template name.
-  errorLayout: 'error',
+		// Name
+		siteAuthor: 'Septian Adi',
 
-  // The URL that will be the default view that will initially load
-  // For example, this could a list view or a could be a specific view
-  // like a single page.
-  defaultView: 'posts',
+		// Navigation items
+		siteNavItems: [
+			{ name: 'Github', href: 'https://github.com/mcseptian/', newWindow: false},
+			{ name: 'About'}
+		],
 
-  // These are the types of content to load. Each type name is a directory or
-  // folder where the files, pages or posts are located. Each type has a list
-  // and single layout template that will determine how the file will be rendered.
-  types: [
-    {
-    // for example, layouts/post-list.html
-    name: 'posts',
-      layout: { list: 'post-list', single: 'post' },
-    },
-    {
-      name: 'pages',
-      layout: { list: 'page-list', single: 'page' },
-    },
-  ],
-};
+		// Posts folder name
+		postsFolder: 'posts',
 
-// Initialize CMS.js
-var blog = new CMS(config);
+		// Homepage posts snippet length
+		postSnippetLength: 120,
+
+		// Pages folder name
+		pagesFolder: 'pages',
+
+		// Site fade speed
+		fadeSpeed: 300,
+
+		// Site footer text
+		footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
+
+		// Mode 'Github' for Github Pages, 'Apache' for Apache server. Defaults
+		// to Github
+		mode: 'Github',
+
+		// If Github mode is set, your Github username and repo name. Defaults
+		// to Github pages branch (gh-pages)
+		githubUserSettings: {
+			username: 'mcseptian',
+			repo: 'https://github.com/mcseptian/hello'
+		}
+
+	});
+
+	// Markdown settings
+	marked.setOptions({
+		renderer: new marked.Renderer(),
+		gfm: true,
+		tables: true,
+		breaks: false,
+		pedantic: false,
+		sanitize: true,
+		smartLists: true,
+		smartypants: false
+	});
+
+});
